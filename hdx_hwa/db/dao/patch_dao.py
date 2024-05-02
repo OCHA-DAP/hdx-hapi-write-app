2be317785ebdd0e5d762230b9e5d45657ba6dbef
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-05-02 10:06:28 +0100

    HDX-9752 implement and test get_highest_patch_number

--- hdx_hwa/db/dao/patch_dao.py
+++ hdx_hwa/db/dao/patch_dao.py
@@ -1,5 +1,5 @@
 import logging
-
+import sqlalchemy
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import select
 
@@ -12,7 +12,7 @@ from hapi_schema.db_patch import DBPatch
 logger = logging.getLogger(__name__)
 
 # Functions to implement
-# get patch by id
+# get patch by id - DONE
 # create new patch
 # update a patch
 # find next patch to execute → failed or discovered patch with lowest sequence number
@@ -20,13 +20,7 @@ logger = logging.getLogger(__name__)
 
 
 def get_patch_by_id(id: int, db: Session = None) -> list[DBPatch]:
-    if db is None:
-        engine = create_engine(
-            get_config().SQL_ALCHEMY_PSYCOPG2_DB_URI,
-        )
-        # print(get_config().SQL_ALCHEMY_PSYCOPG2_DB_URI, flush=True)
-        db = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+    db = get_db_connection(db)
     query = select(DBPatch)
     query = query.where(DBPatch.id == id)
     result = db.execute(query)
@@ -36,3 +30,26 @@ def get_patch_by_id(id: int, db: Session = None) -> list[DBPatch]:
     logger.info(f'Retrieved {len(patch_data)} rows from the database')
 
     return patch_data
+
+
+def get_highest_sequence_number(db: Session) -> int:
+    db = get_db_connection(db)
+    query = select(sqlalchemy.func.max(DBPatch.patch_sequence_number))
+    result = db.execute(query)
+    sequence_number = result.scalars().all()
+
+    logger.debug(f'Executing SQL query: {query}')
+    logger.info(f'Got result {sequence_number} rows from the database')
+
+    return sequence_number[0]
+
+
+def get_db_connection(db: Session) -> Session:
+    if db is None:
+        engine = create_engine(
+            get_config().SQL_ALCHEMY_PSYCOPG2_DB_URI,
+        )
+        # print(get_config().SQL_ALCHEMY_PSYCOPG2_DB_URI, flush=True)
+        db = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    return db
--- tests/test_patch_dao.py
+++ tests/test_patch_dao.py
@@ -1,6 +1,6 @@
 import logging
 from sqlalchemy.orm import sessionmaker, Session
-from hdx_hwa.db.dao.patch_dao import get_patch_by_id
+from hdx_hwa.db.dao.patch_dao import get_patch_by_id, get_highest_sequence_number
 
 
 def test_get_patch_by_id(log: logging.Logger, session_maker: sessionmaker[Session]):
@@ -10,3 +10,10 @@ def test_get_patch_by_id(log: logging.Logger, session_maker: sessionmaker[Sessio
     assert len(result) == 1
     assert result[0].id == 1
     assert result[0].commit_hash == '66e7e589a1224a1832ba7360817dda7a7d9313cf'
+
+
+def test_get_highest_sequence_number(log: logging.Logger, session_maker: sessionmaker[Session]):
+    session = session_maker()
+    result = get_highest_sequence_number(db=session)
+
+    assert result == 3
