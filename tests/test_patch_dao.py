d59de6d80d679eba50345b59dd6995160f9f608b
Alexandru Gartner <alexandru-m-g@users.noreply.github.com>
2024-05-07 16:19:43 +0000

    HDX-9752 autoclosing db session at the end of test

--- tests/test_patch_dao.py
+++ tests/test_patch_dao.py
@@ -1,6 +1,6 @@
 import datetime
 import logging
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import Session
 
 from hapi_schema.db_patch import StateEnum, DBPatch
 from hdx_hwa.db.dao.patch_dao import (
@@ -13,38 +13,33 @@ from hdx_hwa.db.dao.patch_dao import (
 )
 
 
-def test_get_highest_sequence_number(log: logging.Logger, session_maker: sessionmaker[Session]):
-    session = session_maker()
-    result = get_highest_sequence_number(db=session)
+def test_get_highest_sequence_number(log: logging.Logger, db_session: Session):
+    result = get_highest_sequence_number(db=db_session)
 
     assert result == 3
 
 
-def test_get_most_recent_patch(log: logging.Logger, session_maker: sessionmaker[Session]):
-    session = session_maker()
-    result = get_most_recent_patch(db=session)
+def test_get_most_recent_patch(log: logging.Logger, db_session: Session):
+    result = get_most_recent_patch(db=db_session)
 
     assert result.state == StateEnum.failed
     assert result.execution_date == datetime.datetime(2023, 1, 3, 0, 0)
 
 
-def test_insert_new_patch_failure_type(log: logging.Logger, session_maker: sessionmaker[Session]):
-    session = session_maker()
-    status, _ = insert_new_patch(1, db=session)
+def test_insert_new_patch_failure_type(log: logging.Logger, db_session: Session):
+    status, _ = insert_new_patch(1, db=db_session)
 
     assert status == 'failure: wrong type'
 
 
-def test_insert_new_patch_failure_integrity(log: logging.Logger, session_maker: sessionmaker[Session]):
-    session = session_maker()
+def test_insert_new_patch_failure_integrity(log: logging.Logger, db_session: Session):
     new_patch = DBPatch()
-    status, _ = insert_new_patch(new_patch, db=session)
+    status, _ = insert_new_patch(new_patch, db=db_session)
 
     assert status == 'failure: integrity error'
 
 
-def test_insert_new_patch_success(log: logging.Logger, session_maker: sessionmaker[Session]):
-    session = session_maker()
+def test_insert_new_patch_success(log: logging.Logger, db_session: Session):
     new_patch = DBPatch(
         patch_sequence_number=4,
         commit_hash='554f18a92cf6a23a14e0f29356a6dec150f651gg',
@@ -55,27 +50,25 @@ def test_insert_new_patch_success(log: logging.Logger, session_maker: sessionmak
         patch_permalink_url='https://github.com/OCHA-DAP/hapi-patch-repo/blob/554f18a92cf6a23a14e0f29356a6dec150f651ff/2024/01/hapi_patch_4_hno.json',
         state=StateEnum.discovered,
     )
-    status, inserted_id = insert_new_patch(new_patch, db=session)
+    status, inserted_id = insert_new_patch(new_patch, db=db_session)
 
     assert status == 'success'
-    result = get_highest_sequence_number(db=session)
+    result = get_highest_sequence_number(db=db_session)
     assert result == 4
 
-    result = get_patch_by_id(inserted_id, db=session)
+    result = get_patch_by_id(inserted_id, db=db_session)
     assert result.commit_hash == '554f18a92cf6a23a14e0f29356a6dec150f651gg'
 
 
-def test_get_next_patch_to_execute(log: logging.Logger, session_maker: sessionmaker[Session]):
-    session = session_maker()
-    result = get_next_patch_to_execute(db=session)
+def test_get_next_patch_to_execute(log: logging.Logger, db_session: Session):
+    result = get_next_patch_to_execute(db=db_session)
 
     assert result.patch_sequence_number == 2
     assert result.state == StateEnum.failed
 
 
-def test_get_last_executed_patch(log: logging.Logger, session_maker: sessionmaker[Session]):
-    session = session_maker()
-    result = get_last_executed_patch(db=session)
+def test_get_last_executed_patch(log: logging.Logger, db_session: Session):
+    result = get_last_executed_patch(db=db_session)
 
     assert result.patch_sequence_number == 1
     assert result.state == StateEnum.executed
