29d715e0fc7d44e302ff0a372630a7da8f344092
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2024-06-13 15:14:51 +0300

    HDX-9734 slack integration

--- hdx_hwa/config/config.py
+++ hdx_hwa/config/config.py
@@ -1,6 +1,8 @@
-from dataclasses import dataclass
 import logging
 import os
+
+from dataclasses import dataclass
+from typing import Optional
 
 from hdx_hwa.config.helper import create_pg_uri_from_env_without_protocol
 
@@ -14,7 +16,9 @@ class Config:
     HWA_PATCH_REPO_URL: str
     HWA_PATCH_BRANCH_NAME: str
     HWA_PATCH_FOLDER: str
-    HWA_PATCH_TOKEN: str
+    HWA_PATCH_TOKEN: Optional[str]
+    HWA_SLACK_NOTIFICATION_CHANNEL: str
+    HAPI_SLACK_CENTRE_ACCESS_TOKEN: Optional[str]
 
 
 CONFIG = None
@@ -34,6 +38,9 @@ def get_config() -> Config:
             HWA_PATCH_BRANCH_NAME=os.getenv('HWA_PATCH_BRANCH_NAME', 'main'),
             HWA_PATCH_TOKEN=os.getenv('HWA_PATCH_TOKEN', None),
             HWA_PATCH_FOLDER=os.getenv('HWA_PATCH_FOLDER', 'database/csv'),
+            HWA_SLACK_NOTIFICATION_CHANNEL=
+                os.getenv('HWA_SLACK_NOTIFICATION_CHANNEL', 'topic-hapi-testing-notifications'),
+            HAPI_SLACK_CENTRE_ACCESS_TOKEN=os.getenv('HAPI_SLACK_CENTRE_ACCESS_TOKEN', None),
         )
 
     return CONFIG
--- hdx_hwa/main.py
+++ hdx_hwa/main.py
@@ -4,12 +4,14 @@ from hdx_hwa.db.services import end_db_session, get_latest_executed_patch_hash_f
 
 from hdx_hwa.engine.services import execute_patch
 from hdx_hwa.patch_repo.services import discover_patches_from_repo
+from hdx_hwa.util.slack_wrapper import SlackClientWrapper
 from hdx_hwa.util.timer import Timer
 
 logger = logging.getLogger(__name__)
 
 
 def process():
+    slack_wrapper = SlackClientWrapper()
     import_proces_timer = Timer('Import timer', in_millis=False)
     list_of_patches = discover_patches_from_repo()
     if list_of_patches:
@@ -20,25 +22,35 @@ def process():
             try:
                 patch_hash = get_latest_executed_patch_hash_for_target_from_db(discovered_patch.patch_target)
                 if patch_hash != discovered_patch.patch_hash:
-                    logger.info(
-                        f'Loading patch data for {discovered_patch.patch_target} '
+                    # logger.info(
+                    #     f'Loading patch data for {discovered_patch.patch_target} '
+                    #     f'and commit hash {discovered_patch.commit_hash}'
+                    # )
+                    import_proces_timer.next(
+                        f'Loading data for {discovered_patch.patch_target} '
                         f'and commit hash {discovered_patch.commit_hash}'
                     )
-                    import_proces_timer.next(f'Started loading data for {discovered_patch.patch_target}')
                     execute_patch(discovered_patch)
-                    import_proces_timer.next(f'Finished loading data for {discovered_patch.patch_target}')
-                    logger.info(
-                        f'Finished loading data for {discovered_patch.patch_target} '
+                    finished_loading_message = f'Finished loading data for {discovered_patch.patch_target} ' \
                         f'and commit hash {discovered_patch.commit_hash}'
-                    )
+                    import_proces_timer.next(finished_loading_message)
+                    slack_wrapper.post_to_slack_channel(finished_loading_message)
+                    # logger.info(
+                    #     f'Finished loading data for {discovered_patch.patch_target} '
+                    #     f'and commit hash {discovered_patch.commit_hash}'
+                    # )
                 else:
                     logger.info(
                         f'Patch data for {discovered_patch.patch_target} is already loaded. '
                         f'Commit hash: {discovered_patch.commit_hash}'
                     )
             except Exception as e:
-                logger.error(f'Error while processing patch for target: {discovered_patch.patch_target}: {str(e)}')
+                message = f'Error while processing patch for target: {discovered_patch.patch_target}: {str(e)}'
+                logger.error(message)
+                slack_wrapper.post_to_slack_channel(message)
             finally:
                 end_db_session()
 
-    import_proces_timer.next('Finished import process')
+    process_finished_message = 'Finished import process'
+    import_proces_timer.next(process_finished_message)
+    slack_wrapper.post_to_slack_channel(process_finished_message)
