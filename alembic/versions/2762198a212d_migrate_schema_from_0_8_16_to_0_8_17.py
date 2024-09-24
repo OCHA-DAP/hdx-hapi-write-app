4eb08bf95e9e706d943b81532c51a05764adaf56
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-09-24 07:16:07 +0100

    HDX-10177 Manually edit Alembic step to include new primary key

--- alembic/versions/2762198a212d_migrate_schema_from_0_8_16_to_0_8_17.py
+++ alembic/versions/2762198a212d_migrate_schema_from_0_8_16_to_0_8_17.py
@@ -24,6 +24,9 @@ def upgrade() -> None:
     op.alter_column('idps_vat', 'reporting_round',
                existing_type=sa.INTEGER(),
                nullable=False)
+    op.drop_constraint('idps_vat_pkey', 'idps_vat', type_='primary')
+    op.create_primary_key('idps_vat_pkey', 'idps_vat', ['admin2_ref','assessment_type',
+                                                        'reporting_round','operation','reference_period_start'])
     # ### end Alembic commands ###
 
 
@@ -33,4 +36,6 @@ def downgrade() -> None:
                existing_type=sa.INTEGER(),
                nullable=True)
     op.drop_column('idps_vat', 'operation')
+    op.drop_constraint('idps_vat_pkey', 'idps_vat', type_='primary')
+    op.create_primary_key('idps_vat_pkey', 'idps_vat', ['admin2_ref','assessment_type'])
     # ### end Alembic commands ###
